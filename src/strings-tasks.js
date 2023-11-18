68a904af7d5859036599160c6dad7d09a7f0ff30
Naya Naya <naya252naya@gmail.com>
2023-11-18 21:53:32 +0300

    feat: add solution to formatTime

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -245,8 +245,10 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const min = minutes.toString();
+  const sec = seconds.toString();
+  return `${min.padStart(2, '0')}:${sec.padStart(2, '0')}`;
 }
 
 /**
