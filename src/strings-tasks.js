a738f445d5caa025e6d2a365ff5644c03970501f
Naya Naya <naya252naya@gmail.com>
2023-11-18 23:05:24 +0300

    feat: add solution to isPalindrome

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -348,8 +348,13 @@ function countVowels(str) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const cleanStr = str
+    .toLowerCase()
+    .replaceAll(' ', '')
+    .split('')
+    .filter((el) => el !== ',' && el !== '!' && el !== '?');
+  return cleanStr.join('') === cleanStr.reverse().join('');
 }
 
 /**
